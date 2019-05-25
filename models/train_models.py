9f54d1cb258dac31bdd91c7f9fd079f908580b1b
Rishi Subramanian <risubramanian@ucdavis.edu>
2019-05-24 21:17:53 -0700

    updated training scripts

--- models/train_models.py
+++ models/train_models.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python3
+
 import subprocess
 import multiprocessing
 import os
+import json
+import io
 
 num_cpu = multiprocessing.cpu_count()
 
-gpu_name = subprocess.check_output(["nvidia-smi", "--query-gpu=name", "--format=csv,noheader"])
+gpu_name = subprocess.check_output(["nvidia-smi", "--query-gpu=name", "--format=csv,noheader"]).decode()
 is_tesla = "tesla" in gpu_name.lower()
 
 training_options = [
     "--log-interval", "100",
     "--log-format", "json",
-    "--num_workers", str(num_cpu),
+    "--num-workers", str(num_cpu),
     "--skip-invalid-size-inputs-valid-test",
     "--max-epoch", "10",
     "--optimizer", "adam",
@@ -56,19 +60,44 @@ dynamicconv_model = [
     "--decoder-conv-type", "dynamic"
 ]
 
-model_names = ["seq2seq", "transformer", "dynamicconv"]
-datasets = ["limited_vocabulary", "bpe"]
+model_names = [
+    # "seq2seq",
+    "transformer"
+    # "dynamicconv"
+]
+
+model_params = [
+    # seq2seq_model,
+    transformer_model
+    # dynamicconv_model
+]
+
+datasets = [
+    "limited_vocab",
+    "bpe"
+]
 
 command_list = {}
 
-for model_name, model in zip(model_names, [seq2seq_model, transformer_model, dynamicconv_model]):
+for model_name, model in zip(model_names, model_params):
     for dataset in datasets:
         data_path = [os.path.join("../data", dataset)]
         checkpoint_dir = os.path.join("checkpoints", model_name, dataset)
-        params = ["fairseq_train"] data_path + training_options + model + ["--save-dir", checkpoint_dir]
-        config_name = "_".join(model_name, dataset)
-        command_list[config_name] = (params)
+        params = ["fairseq-train"] + data_path + training_options + model + ["--save-dir", checkpoint_dir]
+        config_name = "_".join([model_name, dataset])
+        command_list[config_name] = params
+        # print(params)
 
 for name, command in command_list.items():
-    with open(name = "_log.json", "w") as log_file:
+    print(f"Training: {name}")
+    with open(name + "_log.json", "wb") as log_file:
         training_proc = subprocess.run(command, stdout=log_file)
+        # for line in io.TextIOWrapper(training_proc.stdout, encoding="utf-8"):
+        #     print(line)
+        #     if line.strip()[0] == "{":
+        #         log_file.write(line)
+        #         try:
+        #             log_line = json.loads(line)
+        #             print(f"Epoch: {log_line['Epoch']}\tUpdate: {log_line['Update']}")
+        #         except json.JSONDecodeError:
+        #             pass
